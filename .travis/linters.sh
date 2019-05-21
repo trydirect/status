95fdabbdfd2ff881f8b328df03ab72188e5bd100
vsilent <jabberroid@gmail.com>
2019-05-21 15:52:07 +0300

    docker security check added

--- .travis/linters.sh
+++ .travis/linters.sh
@@ -5,3 +5,6 @@ docker pull hadolint/hadolint:v1.3.0
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 find ./ -name Dockerfile -print0 | xargs -0 -L1 $DIR/docker-linter.sh
+
+docker pull docker/docker-bench-security
+$DIR/docker-security.sh
