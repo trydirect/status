5cdcfe879474eb6bbecc92407432a04026d4098e
vsilent <jabberroid@gmail.com>
2019-05-24 14:15:28 +0300

    test title is presented

--- tests.py
+++ tests.py
@@ -10,13 +10,9 @@ time.sleep(10)  # we expect all containers are up and running in 20 secs
 
 web = client.containers.get('status')
 print(web.logs())
-# assert 'spawned uWSGI master process' in web.logs()
-# assert 'spawned uWSGI worker 1' in web.logs()
-# assert 'spawned uWSGI worker 2' in web.logs()
-# assert 'spawned uWSGI worker 3' in web.logs()
-# assert 'spawned uWSGI worker 4' in web.logs()
+assert 'Running on http://0.0.0.0:5000' in web.logs()
 assert web.status == 'running'
 response = requests.get("http://localhost:5000")
-assert response.status_code == 200
-# assert "" in response.text
 print(response.text)
+assert response.status_code == 200
+assert "Status Panel" in response.text
