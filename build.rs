742cfca8305ee14e8719849a8a56cc843cdb5dda
vsilent <jabberroid@gmail.com>
2026-04-17 20:56:57 +0300

    protoc deps

--- build.rs
+++ build.rs
@@ -1,5 +1,8 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("cargo:rerun-if-changed=proto/pipe.proto");
+    // Vendor protoc so builds work without a system-installed protoc
+    let _protoc = protoc_bin_vendored::protoc_bin_path().expect("vendored protoc not found");
+    std::env::set_var("PROTOC", &_protoc);
     tonic_build::configure()
         .build_server(false)
         .build_client(true)
